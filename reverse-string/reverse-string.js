23c01bb943eb32c12d63ee7aadebf1be579e088b
Jonathan Cabezas <jonathan.cabezas@getapp.com>
2018-07-10 11:29:19 +0200

    exercism javascript 5 ex

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,3 +1,9 @@
-class ReverseString{
-  
+function reverseString(str){
+  var revStr = '';
+  for(var i = str.length - 1; i >= 0; i--){
+    revStr += str[i];
+  }
+  return revStr;
 }
+
+module.exports = reverseString;
